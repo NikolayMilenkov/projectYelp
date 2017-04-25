d926b9cf9b2ae26738b270368dc0ee160c41f849
Nikolay Milenkov <nikolito007@Nikolays-MacBook-Air.local>
2017-04-25 10:48:39 +0300

    post request edited

--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,23 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
    
-    var usersCollection = req.db.get('users');
+   var usersCollection = req.db.get('users');
+   usersCollection.find({username: "rado", password: "rado"},{}).then(function(data) {
+       res.json(data);
+    }).catch(function(err){
+        res.json(500, err);
+    });
+    /*var usersCollection = req.db.get('users');
     usersCollection.find({})
     .then(function(data) {
         res.json(data);
     }).catch(function(err) {
         res.json(500, err);
-    });
+    });*/
+    
 });
 router.post('/', function(req, res, next) {
+     var bool = true;
     var username = req.body.username;
     var password = req.body.password;
     var db = req.db;
@@ -21,10 +29,11 @@ router.post('/', function(req, res, next) {
         .then(function (data) {
             if (data.length > 0) {
                // req.session.userId = data[0]._id;
-                res.redirect('/');
-            } /*else {
-                res.render('login', { message: 'Are probvai pak moi chovek' });
-            }*/
+               res.end(JSON.stringify(bool));
+            } else {
+                res.end("check failed!");
+                //res.render('login', { message: 'Are probvai pak moi chovek' });
+            }
         });
    
   /*  usersCollection.find({})
