03f930b9cd584ed5ebe5a85a7132596dc52757cf
Radoslav Panayotov <radoslav_pan@abv.bg>
2017-04-26 10:28:42 +0300

    commit

---
+++ routes/createUsers.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+
+//    var usersCollection = req.db.get('users');
+//    usersCollection.find({username: "rado", password: "rado"},{}).then(function(data) {
+//        res.json(data);
+//     }).catch(function(err){
+//         res.json(500, err);
+//     });
+//     /*var usersCollection = req.db.get('users');
+//     usersCollection.find({})
+//     .then(function(data) {
+//         res.json(data);
+//     }).catch(function(err) {
+//         res.json(500, err);
+//     });*/
+
+// });
+
+
+function CreateUser(username, password, email, type) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.type = type;
+};
+
+router.post('/', function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var email = req.body.email;
+    var type = req.body.type;
+    var newUser = new CreateUser(username, password, email, type)
+    var db = req.db;
+    var users = db.get('users');
+    users.find({ username: username })
+        .then(function (data, res, newUser) {
+            console.log(data);
+            var bool = false;
+            if (data.length > 0) {
+                res.end(JSON.stringify(bool));
+            } else {
+                users.find({ email: email }).then(function (data) {
+                    console.log("user = " + newUser);
+                    if (data.length > 0) {
+                        res.end(JSON.stringify(bool));
+                    } else {
+                        bool = true;
+                        users.insert(newUser);
+                        res.end(JSON.stringify(bool));
+                        // req.session.userId = data[0]._id;
+                    }
+                });
+            }
+        });
+
+    /*  usersCollection.find({})
+      .then(function(data) {
+          res.json(data);
+      }).catch(function(err) {
+          res.json(500, err);
+      });*/
+});
+
+module.exports = router;
