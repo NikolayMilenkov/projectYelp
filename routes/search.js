8d867efe1aab5837f83d7e5fc25ca7f3d3d9bcd8
Radoslav Panayotov <radoslav_pan@abv.bg>
2017-04-26 11:03:42 +0300

    comited

---
+++ routes/search.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+// GET home page. 
+router.get('/', function(req, res, next) {
+  req.session.destroy();
+});
+
+router.post('/',function(req, res, next){
+    var usersCollection = req.db.get('businesses');
+    
+})
+module.exports = router;
